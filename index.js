af707992522f15146250063844b06fc2a4ac8a8e
James Lenahan <james.k.lenahan@gmail.com>
2020-04-07 15:37:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+class Polygon{
+
+    constructor(intArray){
+        this.sideLengths = intArray
+    }
+
+    get countSides(){
+        return this.sideLengths.length
+    }
+
+    get perimeter(){
+        return this.sideLengths.reduce( (sum, length) => { return sum + length }, 0);
+    }
+}
